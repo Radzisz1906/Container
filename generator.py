a2699092b73ca1796115a85e41bede69a04786e8
Krzysztof Radziszewski <krzych@pc154.home>
2020-05-27 19:30:31 +0200

    nie chce mi sie

--- generator.py
+++ generator.py
@@ -1,47 +1,23 @@
 import random
 
-print("A, stala ladownosc")
 
-conteiners = int(input("Numbers of conteiners: "))
-max_conteiner_value = int(input("Max weight of conteiner: "))
-ship_capacity = int(conteiners * max_conteiner_value * 0.3)
+def gen(conteiners, max_conteiner_value):
+    print("A, stala ladownosc")
+    ship_capacity = max_conteiner_value
 
-plik = open("statkiA.txt", "w")
-ship_line = str(ship_capacity), " ", str(conteiners)
+    plik = open("statkiA.txt", "w")
+    ship_line = str(ship_capacity), " ", str(conteiners)
 
-plik.write(str(ship_capacity))
-plik.write(" ")
-plik.write(str(str(conteiners)))
-plik.write(str("\n"))
-
-for i in range(conteiners):
-    plik.write(str(str(i + 1)))
+    plik.write(str(ship_capacity))
     plik.write(" ")
-    plik.write(str(random.randint(1, max_conteiner_value)))
-    plik.write(" ")
-    plik.write(str(random.randint(1, 100)))
-    plik.write("\n")
-plik.close()
+    plik.write(str(str(conteiners)))
+    plik.write(str("\n"))
 
-print("B, stala liczba kontenerow")
-
-conteiners = int(input("Numbers of conteiners: "))
-max_conteiner_value = int(input("Max weight of conteiner: "))
-ship_capacity = int(conteiners * max_conteiner_value * 0.05)
-
-plik = open("statkiB.txt", "w")
-ship_line = str(ship_capacity), " ", str(conteiners)
-
-plik.write(str(ship_capacity))
-plik.write(" ")
-plik.write(str(str(conteiners)))
-plik.write(str("\n"))
-
-for i in range(conteiners):
-    plik.write(str(str(i + 1)))
-    plik.write(" ")
-    plik.write(str(random.randint(1, max_conteiner_value)))
-    plik.write(" ")
-    plik.write(str(random.randint(1, 100)))
-    plik.write("\n")
-plik.close()
+    for i in range(conteiners):
+        plik.write(str(str(i + 1)))
+        plik.write(" ")
+        plik.write(str(random.randint(1, max_conteiner_value)))
+        plik.write(" ")
+        plik.write(str(random.randint(1, 100)))
+        plik.write("\n")
+    plik.close()
--- kontener.py
+++ kontener.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+import generator
+from generator import gen
 
 
 class plecak():
@@ -12,24 +14,57 @@ class plecak():
         d = defaultdict(list)
         sor = []
         su = 0
+        war = 0
         for i in self.kontenery:
             d[i[2] / i[1]] = [i[0], i[1], i[2]]
         for i in sorted(d.keys(), reverse=True):
             if su + d[i][1] <= self.ladownosc:
                 sor.append(d[i][0])
                 su += d[i][1]
+                war += d[i][2]
             else:
                 break
         print(sor)
+        print(war)
 
 
+def dynamic(waga, wartosc, pojemnosc, l_kontenerow):
+    tabela = [[0 for x in range(pojemnosc + 1)]
+              for y in range(l_kontenerow + 1)]
+    for i in range(1, l_kontenerow + 1):
+        for j in range(1, pojemnosc + 1):
+            if j < waga[i - 1]:
+                tabela[i][j] = tabela[i - 1][j]
+            else:
+                tabela[i][j] = max(tabela[i - 1][j], tabela[i - 1]
+                                   [j - waga[i - 1]] + wartosc[i - 1])
+    return tabela[l_kontenerow][pojemnosc]
+
+
+print("1.Generuj plik i działaj. \n2.Działaj na istniejącym. \n")
+wyb = int(input())
+if wyb == 1:
+    print("Max ladownosc statku: ")
+    ladownosc = int(input())
+    print("Ilosc kontenerow:")
+    ilosc_kont = int(input())
+    gen(ilosc_kont, ladownosc)
 dane = []
+waga = []
+wartosc = []
 plik = open("statkiA.txt")
 i = 0
 for line in plik:
     tmp = line.split()
     tmp = list(map(int, tmp))
     dane.append(tmp)
-p = plecak(dane)
+    if i > 0:
+        waga.append(tmp[1])
+        wartosc.append(tmp[2])
+    i += 1
 plik.close()
+p = plecak(dane)
+print("Dynamiczne:")
+print(dynamic(waga, wartosc, p.ladownosc, p.ilosc))
+print("Zachłanne:")
 p.zach()
